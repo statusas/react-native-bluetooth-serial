089c7b94b8e589883143c4d07f3e1ef636b7524c
BenLovell24 <benjamin_lovell@outlook.com>
2023-02-28 13:35:34 +1100

    Lowered buffer size for faster data flow

--- android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialService.java
+++ android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialService.java
@@ -296,7 +296,7 @@ class RCTBluetoothSerialService {
 
         public void run() {
             Log.i(TAG, "BEGIN mConnectedThread");
-            byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[64];
             int bytes;
 
             // Keep listening to the InputStream while connected
